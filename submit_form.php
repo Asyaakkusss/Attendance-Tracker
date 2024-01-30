9e1d790ffc988511d25e731cd7aa25795f2259b3
Asya <aya29@case.edu>
2024-01-29 23:09:21 -0500

    php file connection info created

--- submit_form.php
+++ submit_form.php
@@ -1,6 +1,23 @@
 <?php
-
+/*
 $servername = "localhost";
 $password = "password"; 
 $firstname = "firstname"; 
 $lastname = "lastname"; 
+$dbname = "attendance_db"
+*/
+//create a connection 
+$serverName = "localhost"; 
+$connectionInfo = array("DB"=>"attendance_db", 
+                        "PW"=>"password", 
+                        "FN"=>"firstname", 
+                        "LN"=> "lastname"); 
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+//check connection 
+if ($conn) {
+    echo "Connection Established";
+} else {
+    echo "Connection Failed"; 
+    die(print_r(sqlsrv_errors(), true));
+}
+?>
